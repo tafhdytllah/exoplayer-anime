72c0a04aedc6efd5eed79a64907847266826a319
Taufik <tafhdytllah@gmail.com>
2020-12-25 06:25:18 +0700

    create exoplayer-anime app

---
+++ app/src/main/java/com/tafh/exoplayeranime/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tafh.exoplayeranime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
